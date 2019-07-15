3e92f94b0b32ba8d5895226598e2224675a3e20c
Elias Baumann <baumaneq@hu-berlin.de>
2019-07-15 23:23:55 +0200

    Initial commit

---
+++ model.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+# TODO we need two different parts:
+# 1. YOLO
+# 2. The no bells and whistles Tracktor
+
+# Also all the necessary helper methods and potentially an extra file for regression
---
+++ run.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+
+from mpi4py import MPI
+
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--lambda', type=float, default=0.95) # basic example on how to use the parser
+
+    args = parser.parse_args()
+    print(args.__dict__)
+
+    # comm = MPI.COMM_WORLD
+    # rank = comm.Get_rank() # debatable wether i want to think about mpi now..., maybe if it works, put in the effort to make it run on mpi
