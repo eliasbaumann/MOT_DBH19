e1751e203a775a0eceb73dce374db0ebd3b8d288
Elias Baumann <baumaneq@hu-berlin.de>
2019-07-16 23:27:46 +0200

    from tutorial

--- model.py
+++ model.py
@@ -1,7 +1,20 @@
 import tensorflow as tf
 
+from feature_extractor import Feature_extractor
 # TODO we need two different parts:
 # 1. YOLO
 # 2. The no bells and whistles Tracktor
 
 # Also all the necessary helper methods and potentially an extra file for regression
+
+class Tracktor(object):
+    
+    def __init__(self):
+        with tf.variable_scope('tracktor'):
+            self.img_in= tf.keras.Input(shape=(None,None))
+            self.box_in = tf.keras.Input(shape=(None,None))
+
+            self.feature_extractor = Feature_extractor() # TODO how do we get something here...
+
+            self.features = 0 # here i want the features
+
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from mpi4py import MPI
 
+from feature_extractor import Feature_extractor
 
 
 
@@ -11,8 +12,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--lambda', type=float, default=0.95) # basic example on how to use the parser
 
-    args = parser.parse_args()
-    print(args.__dict__)
-
+    #args = parser.parse_args()
+    #print(args.__dict__)
+    with tf.Session() as sess:
+        asf = Feature_extractor()
     # comm = MPI.COMM_WORLD
     # rank = comm.Get_rank() # debatable wether i want to think about mpi now..., maybe if it works, put in the effort to make it run on mpi
